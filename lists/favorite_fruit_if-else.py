db94fa6fd04cdaa2d63646c905f685987105f30b
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-04-28 18:22:24 +1000

    learnt about if statements

---
+++ lists/favorite_fruit_if-else.py
@@ -0,0 +1,13 @@
+fav_fruits=["mango","apple","bluebarry","cherry"]
+part_msg="You really like"
+for fruit in fav_fruits:
+    if fruit=="mango":
+        print(f"{part_msg} {fruit}")
+    if fruit=="apple":
+       print(f"{part_msg} {fruit}")
+    if fruit=="bluebarry":
+        print(f"{part_msg} {fruit}")
+    if fruit=="cherry":
+        print(f"{part_msg} {fruit}")
+    if fruit=="kiwi":
+        print(f"{part_msg} {fruit}")
