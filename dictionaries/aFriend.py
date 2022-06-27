0d60a49ffaff7536018bd6fe54f111f68666b12c
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-06-27 15:41:53 +0300

    learning the syntex

--- dictionaries/aFriend.py
+++ dictionaries/aFriend.py
@@ -16,3 +16,8 @@ fav_numbers = {
 
 for key in fav_numbers:
     print(f"{key}'s favourite number is {fav_numbers[key]}")
+
+#looping through a dic based on the book
+for key,value in fav_numbers.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
