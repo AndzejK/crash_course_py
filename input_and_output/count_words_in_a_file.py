a81de7d630acc60b418d331b29ac38639ebe55e8
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-10-02 16:49:15 +0300

    the program that count the words in any txt file

--- input_and_output/count_words_in_a_file.py
+++ input_and_output/count_words_in_a_file.py
@@ -1,11 +1,11 @@
-file_name="txt_a_rand_number_words.txt"
+file_name="txt_a_rand_number_'words.txt"
 
 # Adding in a TRY block to make secure and more readable app for an end-user
 try:
     with open(file_name,encoding="utf-8") as f:
         content=f.read()
 except FileNotFoundError:
-    print(f"Sorry, the file {file_name} does not exist!")
+    print(f"\nSorry, the file {file_name} does not exist!")
 else:
 # Counting an approximate number of the words in the file_name
     # the string method that finds an empty space, technically we're couting the empty spaces
