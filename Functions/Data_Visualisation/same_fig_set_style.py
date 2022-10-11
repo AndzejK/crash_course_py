5ce9491d0362e026d72e136323eb2ce1d7a85817
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-10-11 12:50:48 +0300

    just played with the style of pyplot

--- Functions/Data_Visualisation/same_fig_set_style.py
+++ Functions/Data_Visualisation/same_fig_set_style.py
@@ -1,10 +1,10 @@
 # Matlablib
 import matplotlib.pyplot as plt
 # more about - matplotlib.pyplot - https://matplotlib.org/stable/api/pyplot_summary.html?highlight=pyplot#module-matplotlib.pyplot
-plt.style.available 
+print(plt.style.available )
 input_valus=[1,2,3,4,5,6]  # X values but default :)
 squares = [1, 4, 9, 16, 25,36] #Y values are as attemps/times/order
-plt.style.use("seaborn")
+plt.style.use("seaborn-v0_8-poster")
 fig, ax=plt.subplots() # subplot is FN that can generate one or more plots in the same Figure
                     # fig - represets the entire figure or collection of plot 
 ax.plot(input_valus,squares,linewidth=3)
