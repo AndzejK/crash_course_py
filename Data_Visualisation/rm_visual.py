558178106ac2594d41a81a90135cf08be038d91a
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-10-13 12:47:32 +0300

    plotting a random walk

--- Data_Visualisation/rm_visual.py
+++ Data_Visualisation/rm_visual.py
@@ -4,22 +4,22 @@ from random_walk import RandomWalk
 
 # Make a random walk
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # plot the points in the walk
     plt.style.use("classic")
-    fig, ax=plt.subplots()
+    fig, ax=plt.subplots(figsize=(14,9))
     point_numbers=range(rw.num_points)
-    ax.scatter(rw.x_values,rw.y_values,c=point_numbers, cmap=plt.cm.Blues,edgecolors='none', s=15)
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers, cmap=plt.cm.Blues,edgecolors='none', s=1)
 
     # emphasize  the first  and last points
     ax.scatter(0,0, c="green",edgecolors='none', s=100) 
     ax.scatter(rw.x_values[-1],rw.y_values[-1],c="red", edgecolors='none', s=100)
 
     # Remove the axes 
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    ax.get_xaxis().set_visible(True)
+    ax.get_yaxis().set_visible(True)
     plt.show()
 
     keep_running=input("Make another walk? (y/n): ")
