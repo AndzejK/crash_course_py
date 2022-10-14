e96f4174ea73bfd1555e09f08932af13d1e3f2ba
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-10-14 12:51:30 +0300

    rolling D6 and D10 dice 50k times

--- Data_Visualisation/dice_visual.py
+++ Data_Visualisation/dice_visual.py
@@ -5,11 +5,11 @@ from plotly import offline
 
 # Create D6 x2 - 6-sided die
 die_1=Die()
-die_2=Die()
+die_2=Die(10) # ten sided die
 
 # Make some rolls and store them in the list
 results=[]
-for roll_num in range(1000):
+for roll_num in range(50_000): # rolling 50k times
     result = die_1.roll()+die_2.roll()
     results.append(result)
 
@@ -27,5 +27,5 @@ data=[Bar(x=x_values, y=frequencies)]
 
 x_axis_config= {"title":"Result", "dtick":1}
 y_axis_config= {"title":"Frequency of Result"}
-my_layout=Layout(title="Results of rolling two D6 1000 times",xaxis=x_axis_config,yaxis=y_axis_config)
+my_layout=Layout(title="Results of rolling two D6 and a D10 50000 times",xaxis=x_axis_config,yaxis=y_axis_config)
 offline.plot({"data":data, "layout":my_layout},filename="d6_d6.html")
