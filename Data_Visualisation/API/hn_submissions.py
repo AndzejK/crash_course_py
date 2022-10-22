874eb2e549739b2e30fbe5f0b546ad4d58aaef9f
Andzej Krupoves <andrejkrupovec@gmail.com>
2022-10-22 13:56:56 +0300

    I learnt how to look through diff links and get info from each page

--- Data_Visualisation/API/hn_submissions.py
+++ Data_Visualisation/API/hn_submissions.py
@@ -27,6 +27,7 @@ for submission_id in submission_ids[:30]:
     }
     submission_dicts.append(submission_dict)
 submission_dicts=sorted(submission_dicts,key=itemgetter('comments'),reverse=True)
+print(submission_dicts)
 
 for submission_dic in submission_dicts:
     print(f"\nTitle: {submission_dic['title']}")
